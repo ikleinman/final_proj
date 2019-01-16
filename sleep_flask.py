36bc8282b5a3131899d1602ea27d6da585bae0cc
iris <ikleinman19@gmail.com>
2019-01-15 19:48:40 -0500

    Trial Data

--- sleep_flask.py
+++ sleep_flask.py
@@ -2,16 +2,25 @@ from flask import Flask, request, render_template, redirect, jsonify
 from flask_pymongo import PyMongo
 import json
 import os
+import pandas as pd
 
 # create instance of Flask app
 app = Flask(__name__)
 
+sleep_df = pd.read_csv("final_project.csv")
+jsonfiles = json.loads(sleep_df.to_json())
+
+@app.route("/data")
+def data_load():
+
+    return jsonify(jsonfiles)
+
 # Use flask_pymongo to set up mongo connection
-app.config["MONGO_URI"] = #"mongodb://localhost:27017/wine_app"
-mongo = PyMongo(app)
+#app.config["MONGO_URI"] = "mongodb://localhost:27017/"
+#mongo = PyMongo(app)
 
-data = #our data set.data()
 
+# create our session link to our DataFrame
 
 
 
