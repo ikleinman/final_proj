25a961b02440236fbbed2450bb6a8f6f2d02c514
iris <ikleinman19@gmail.com>
2019-01-22 19:48:37 -0500

    cleaning

--- static/js/submit.js
+++ static/js/submit.js
@@ -10,25 +10,15 @@ $('#btnSend').click(function(event){
       contentType: 'application/json',
       processData: true,
       success: function (response) {
-        console.log(response)  
+        console.log(response)
         $('#form').val(response)
 
-        //   console.log(user_submission)
+
   }
    });
 
 });
 
-// $('#btnSend').click(function(event){
-//     event.preventDefault()
-//     console.log("This was clicked!");
-//     var x = document.getElementById("myDIV");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// });
 
 function myFunction(){
     event.preventDefault()
@@ -50,7 +40,7 @@ function selectResult(){
             x.style.display = "none";
         }
     };
-    
+
     function myFunctionNo(){
         event.preventDefault()
         console.log(prediction);
@@ -61,31 +51,10 @@ function selectResult(){
             x.style.display = "none";
         }
     };
-    // myFunctionNo();
-    // myFunctionYes();
+
     if (prediction == '0'){
         myFunctionYes();
     } else {
         myFunctionNo();
     }
-}
-
-// function myFunctionYes(){
-//     event.preventDefault()
-//     var x = document.getElementById("results_yes");
-//     if (x.style.display === "none") {
-//         x.style.display = "inline";
-//     } else {
-//         x.style.display = "none";
-//     }
-// };
-
-// function myFunctionNo(){
-//     event.preventDefault()
-//     var x = document.getElementById("results_no");
-//     if (x.style.display === "none") {
-//         x.style.display = "inline";
-//     } else {
-//         x.style.display = "none";
-//     }
-// };
+};
